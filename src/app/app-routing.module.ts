ee2df0fba2ef617ce4e8c46595e1f5029685ded0
Youssef Moumni <youssef.moumni1@ibm.com>
2022-08-04 17:46:35 +0100

    add home and services routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'services',
+    component: ServicesComponent
+  },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
