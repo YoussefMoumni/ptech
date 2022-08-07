5b23c0b4df5c60086395889a65b1c21999702e92
HoussamNJ <nejmi.houssam4@gmail.com>
2022-08-07 14:48:36 +0100

    add html of mission and scss of mission

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,9 @@ import { ContactsComponent } from './contacts/contacts.component';
 import { HomeComponent } from './home/home.component';
 import { InfosComponent } from './infos/infos.component';
 import { InternshipsComponent } from './internships/internships.component';
-<<<<<<< HEAD
-import { ServicesComponent } from './services/services.component';
-import { MissionComponent } from './mission/mission.component';
 import { NewsComponent } from './news/news.component';
-=======
 import { LoginComponent } from './login/login.component';
 import { MissionComponent } from './mission/mission.component';
->>>>>>> 6c69d7a77f534e6b2e073b74b3606a9b6f178ca2
 
 const routes: Routes = [
   
@@ -35,21 +30,17 @@ const routes: Routes = [
     path: 'Contacts',
     component: ContactsComponent,
   },
-  {
-<<<<<<< HEAD
-    path: ' mission',
-    component:  MissionComponent,
-  },
+  
   {
     path: 'news',
     component: NewsComponent,
   },
 
-=======
+  {
     path: 'login',
     component: LoginComponent,
   },
->>>>>>> 6c69d7a77f534e6b2e073b74b3606a9b6f178ca2
+
   { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
 
